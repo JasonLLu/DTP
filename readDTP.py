bd4f84628a60d0480d7277d761c35c37c4fcb307
Jason Lu <jasonllu@mit.edu>
2019-01-21 20:43:24 -0500

    added print hello

--- readDTP.py
+++ readDTP.py
@@ -4,4 +4,6 @@ import requests
 
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4/export?format=csv&id=1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4')
 #check for git
+#testing
+print('hello')
 print(df)
