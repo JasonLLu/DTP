c717a7f6f94eca629ef6a23c6d9b9e918b3f783d
kev2010 <46427633+kev2010@users.noreply.github.com>
2019-01-22 11:50:09 -0500

    added strength function

--- MatchingAlg.py
+++ MatchingAlg.py
@@ -5,17 +5,19 @@ import csv
 data = {}  # assuming data format is 'kerberos, can_help list, need_help list'
 matches = {}  # dict mapping kerb to list of possible matches, ordered by factor k
 
-def strength(kerb1, can_help1, need_help1, kerb2, can_help2, need_help2):
+def strength(can_help1, need_help1, can_help2, need_help2):
     '''
-    :param kerb1: first user
     :param can_help1: set of classes kerb1 can help w/
     :param need_help1: set of classes kerb1 needs help w/
-    :param kerb2: second user
     :param can_help2: set of classes kerb2 can help w/
     :param need_help2: set of classes kerb2 needs help w/
     :return: integer between 0 and 1 indicating strength of match between kerb1 and kerb2
     '''
-    pass
+    x = len(can_help1.intersection(need_help2))/len(can_help1.union(need_help2))
+    y = len(need_help1.intersection(can_help2))/len(need_help1.union(can_help2))
+    range = abs(x-y)  # accounts for spread
+
+    return (x+y)/2*(1-range)
 
 with open('user_data.txt') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -25,7 +27,7 @@ with open('user_data.txt') as csv_file:
     for user in data:  # create dict of possible matches
         for poss in data:
             if user != poss:
-                k = strength(user, user[0], user[1], poss, poss[0], poss[1])
+                k = strength(user[0], user[1], poss[0], poss[1])
                 if k > 0.5:  # check if k is strong enough
                     try:  # see if user has been seen already
                         matches[user].append((k, poss))
@@ -36,13 +38,3 @@ with open('user_data.txt') as csv_file:
         matches[user].sort()
 
     print(matches)
-
-
-
-
-
-
-
-
-
-
