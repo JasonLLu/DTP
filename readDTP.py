40dfd59bdcbbda975270400048585510cb477963
Jason Lu <jasonllu@mit.edu>
2019-01-21 20:17:42 -0500

    testin branches

--- readDTP.py
+++ readDTP.py
@@ -3,5 +3,5 @@ from io import StringIO  # got moved to io in python3.
 import requests
 
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4/export?format=csv&id=1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4')
-
+#check for git
 print(df)
