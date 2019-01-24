80ca03fecdfd215a0c3b4c347759be11a667776f
kev2010 <46427633+kev2010@users.noreply.github.com>
2019-01-24 03:07:51 -0500

    updated google form + spreadsheet links

--- readDTP.py
+++ readDTP.py
@@ -2,6 +2,9 @@ import pandas as pd
 from io import StringIO  # got moved to io in python3.
 import requests
 
+# Google Form: https://docs.google.com/forms/d/e/1FAIpQLScgo-RXVqytOKyXCMGfC36GE-tu9a6hGjEbm5MzTH_RRTkuxA/viewform?usp=pp_url
+# Google Response: https://docs.google.com/spreadsheets/d/1wquNQ_ICc298ZLmLD6Of6MdIMa-zr5kQ2N1EyelSBJ4/edit#gid=590454524
+
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4/export?format=csv&id=1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4')
 
 print(df)
