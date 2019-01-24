bd10526827411c436f65092e4103eab2222890e7
Jason Lu <jasonllu@mit.edu>
2019-01-24 10:26:38 -0500

    converts link to csv, still needs testing

--- readDTP.py
+++ readDTP.py
@@ -1,22 +1,24 @@
 import pandas as pd
-from io import StringIO  # got moved to io in python3.
 import requests
-
-data = pd.read_csv('https://docs.google.com/spreadsheets/d/1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4/export?format=csv&id=1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4')
-
-#[Jason, Kevin, ...]
-kerberos = data[data.columns[0]]
-#[[6006,6009], [6008,6009]]
-good = data[data.columns[1]]
-#same as good
-bad = data[data.columns[2]]
-
-
-# ex: [[Jason, 6006, 6036], [Kevin,802,6009]]
-nodeList = []
-for i in range (0, len(data.columns)):
-	nodeList.append(data.loc[i])
+import csv
+#Useful for converting google doc:
+#export?format=csv&id=
 
 
 
 
+csv_content = requests.get('https://docs.google.com/spreadsheets/d/1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4/export?format=csv&id=1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4').text
+
+print(csv_content)
+print('**********')
+
+
+# line_count = 0
+# for row in csv_content:
+#     if line_count == 0:
+#         print(f'Column names are {", ".join(row)}')
+#         line_count += 1
+#     else:
+#         print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+#         line_count += 1
+# print(f'Processed {line_count} lines.')
