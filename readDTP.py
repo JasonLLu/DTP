2907baf8bf4442f8be603923a732bd52295d32ed
Jason Lu <jasonllu@mit.edu>
2019-01-24 10:59:22 -0500

    Function finished, but spreadsheet link needs to be changed

--- readDTP.py
+++ readDTP.py
@@ -6,19 +6,25 @@ import csv
 
 
 
-
+#Grabs data from google sheets as a string
 csv_content = requests.get('https://docs.google.com/spreadsheets/d/1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4/export?format=csv&id=1mEtPd_cNuc8YISlq_rxUc9cXlzZxo4_nkmmcq5CEli4').text
 
-print(csv_content)
-print('**********')
+#Converts string to csv file
+f = open('csv_content.csv','w')
+f.write(csv_content) 
+f.close()
 
 
+
+
+#Debugging code
 # line_count = 0
-# for row in csv_content:
-#     if line_count == 0:
-#         print(f'Column names are {", ".join(row)}')
-#         line_count += 1
-#     else:
-#         print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-#         line_count += 1
-# print(f'Processed {line_count} lines.')
+# with open('csv_content.csv') as csv_reader:
+# 	for row in csv_reader:
+# 		if line_count == 0:
+# 			print(f'Column names are {"".join(row)}')
+# 			line_count += 1
+# 		else:
+# 			print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+# 			line_count += 1
+# 	print(f'Processed {line_count} lines.')
